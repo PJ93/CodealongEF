7451e4760f667437df53440be986bdda30016270
pj93coder <pj93coder@gmail.com>
2018-01-23 13:45:09 +0100

    start of the database

---
+++ CodealongEF/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using CodealongEF.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CodealongEF.Controllers
+{
+    public class HomeController : Controller
+    {
+        PeopleDbContext db = new PeopleDbContext();
+
+
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            List<Person> myList = db.people.ToList();
+
+            return View(myList);
+        }
+
+        public ActionResult Create()
+        {
+            Person me = new Person();
+            me.Name = "Bobbo";
+            me.Age = 99;
+
+            db.people.Add(me); //adds something to a DB (database)
+            db.SaveChanges(); //saves changes (add Bobbo)
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ CodealongEF/Models/PeopleDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CodealongEF.Models
+{
+    public class PeopleDbContext : DbContext
+    {
+        public PeopleDbContext() : base("name=PeopleDbContext")
+        {
+
+        }
+
+        public DbSet<Person> people { get; set; }
+
+    }
+}
